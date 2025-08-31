9338ef5c9bf17317cce42ad9ba2a2eddf7cdefc1
Enrico Borba <enricozb@users.noreply.github.com>
2025-08-31 13:58:41 +0200

    initialize cargo workspace and some deps (#1)
    * initialize cargo workspace and some deps
    
    * remove workspace, move lean-lsp/* to top-level
    
    * add clippy pendantic, remove contents of lib.rs

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "lean-lsp";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.fenix.url = "github:nix-community/fenix/monthly";
+
+  outputs = { self, nixpkgs, flake-utils, fenix }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        toolchain = fenix.packages.${system}.fromToolchainFile {
+          file = ./rust-toolchain.toml;
+          sha256 = "sha256-+9FmLhAOezBZCOziO0Qct1NOrfpjNsXxc/8I0c7BdKE=";
+        };
+      in { devShells.default = pkgs.mkShell { packages = [ toolchain ]; }; });
+}
