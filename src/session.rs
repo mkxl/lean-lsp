fedad74148b74bbffe306ec32f825b4ed4c92e1c
michael <66692526+mkxl@users.noreply.github.com>
2025-09-03 18:11:35 -0400

    set up lake process io

--- src/session.rs
+++ src/session.rs
@@ -5,7 +5,6 @@ use ulid::Ulid;
 
 use crate::{lean_server::LeanServer, utils::Utils};
 
-#[allow(dead_code)]
 pub struct Session {
   key: Ulid,
   lean_server: LeanServer,
@@ -13,8 +12,6 @@ pub struct Session {
 }
 
 impl Session {
-  const MANIFEST_FILE_NAMES: &[&'static str] = &["lakefile.lean", "lakefile.toml"];
-
   pub fn new(lean_path: &Path, lean_server_log_dirpath: Option<&Path>) -> Result<Self, Error> {
     let key = Ulid::new();
     let project_dirpath = Self::project_dirpath(lean_path)?;
@@ -25,21 +22,19 @@ impl Session {
       project_dirpath,
     };
 
-    tracing::info!(message = "new session", project_dirpath = %session.project_dirpath.display());
+    tracing::info!(message = "new session", key = %session.key, project_dirpath = %session.project_dirpath.display());
 
     session.ok()
   }
 
   fn project_dirpath(lean_path: &Path) -> Result<PathBuf, Error> {
     for ancestor_path in lean_path.ancestors() {
-      for manifest_file_name in Self::MANIFEST_FILE_NAMES {
-        let mut manifest_filepath = ancestor_path.with_file_name(manifest_file_name);
+      let mut manifest_filepath = ancestor_path.with_file_name("lake-manifest.json");
 
-        if manifest_filepath.is_file() {
-          manifest_filepath.pop();
+      if manifest_filepath.is_file() {
+        manifest_filepath.pop();
 
-          return manifest_filepath.ok();
-        }
+        return manifest_filepath.ok();
       }
     }
 
