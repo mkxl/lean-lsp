3638bfe0977c1dd42ce758c2591cdfd05f48ab53
Enrico Zandomeni Borba <enricozb@gmail.com>
2025-10-12 17:45:03 +0200

    bump rust version + fix nix shell

--- flake.nix
+++ flake.nix
@@ -2,15 +2,37 @@
   description = "lean-lsp";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.fenix.url = "github:nix-community/fenix/monthly";
+  inputs.fenix = {
+    url = "github:nix-community/fenix/monthly";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
 
-  outputs = { self, nixpkgs, flake-utils, fenix }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      fenix,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        toolchain = fenix.packages.${system}.fromToolchainFile {
+        rust-toolchain = fenix.packages.${system}.fromToolchainFile {
           file = ./rust-toolchain.toml;
-          sha256 = "sha256-+9FmLhAOezBZCOziO0Qct1NOrfpjNsXxc/8I0c7BdKE=";
+          sha256 = "sha256-SJwZ8g0zF2WrKDVmHrVG3pD2RGoQeo24MEXnNx5FyuI=";
         };
-      in { devShells.default = pkgs.mkShell { packages = [ toolchain ]; }; });
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.pkg-config
+            pkgs.openssl
+
+            rust-toolchain
+          ];
+        };
+        formatter = pkgs.nixfmt;
+      }
+    );
 }
