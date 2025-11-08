cdcac0f5697e95f3210e0be3ba198d590318f8de
michael <66692526+mkxl@users.noreply.github.com>
2025-11-07 22:06:32 -0500

    Revert "add websocket endpoint (#30)"
    This reverts commit ebe3dbd22aa0b0e52cd2a988faff41945ffcae41.

--- src/session_set.rs
+++ src/session_set.rs
@@ -1,7 +1,6 @@
 use std::path::PathBuf;
 
 use anyhow::Error as AnyhowError;
-use derive_more::From;
 use mkutils::Utils;
 use tokio::{sync::mpsc::UnboundedSender as MpscUnboundedSender, task::JoinHandle};
 use ulid::Ulid;
@@ -10,19 +9,20 @@ use crate::{
   commands::{NewSessionCommand, SessionSetCommand},
   session::Session,
   session_set_runner::SessionSetRunner,
+  types::SessionSetStatus,
 };
 
-#[derive(Clone, From)]
 pub struct SessionSet {
   commands: MpscUnboundedSender<SessionSetCommand>,
+  join_handle: JoinHandle<Result<(), AnyhowError>>,
 }
 
 impl SessionSet {
-  pub fn new() -> (Self, JoinHandle<Result<(), AnyhowError>>) {
+  pub fn new() -> Self {
     let (commands, runner_commands) = tokio::sync::mpsc::unbounded_channel();
     let join_handle = SessionSetRunner::new(runner_commands).run().spawn_task();
 
-    commands.convert::<Self>().pair(join_handle)
+    Self { commands, join_handle }
   }
 
   #[tracing::instrument(skip_all)]
@@ -42,5 +42,25 @@ impl SessionSet {
 
   pub async fn get_session(&self, session_id: Option<Ulid>) -> Result<Session, AnyhowError> {
     crate::macros::run_command!(self, SessionSetCommand::GetSession, session_id)
+  }
+
+  pub async fn status(&self) -> Result<SessionSetStatus, AnyhowError> {
+    let session_set = self.join_handle.is_finished().into();
+    let sessions = self
+      .get_sessions()
+      .await?
+      .iter()
+      .map(Session::status)
+      .try_join_all()
+      .await?;
+    let session_set_status = SessionSetStatus::new(session_set, sessions);
+
+    session_set_status.ok()
+  }
+}
+
+impl Default for SessionSet {
+  fn default() -> Self {
+    Self::new()
   }
 }
